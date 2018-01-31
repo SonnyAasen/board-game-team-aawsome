624d55e72f0f56ef5e2cff165ba276d1d4e89e1a
mmmolin78 <mmmolin78@gmail.com>
2018-01-31 11:17:53 +0100

    More tests

--- Scr/OnlineLudoGame/GameengineTests/UnitTest1.cs
+++ Scr/OnlineLudoGame/GameengineTests/UnitTest1.cs
@@ -51,18 +51,26 @@ namespace GameengineTests
             int gameID = ActiveGame.Game[0].GameID;
             //Assert
             Assert.AreEqual(2, gameID);
-        }
+        }        
 
         [TestMethod]
         public void GetSessionTest()
         {
+            //Arrange            
             User TestUser = new User
             {
                 Name = "test",
-                PlayerID = "randomCookieValue",
+                PlayerID = "test",
                 Side = "O",
                 Email = "test@test.se"
             };
+            Lobby.CreateGame(2, TestUser);            
+            Lobby.FindGame(TestUser);
+            //Act
+            GameSession gameSession1 = ActiveGame.Game[0];
+            GameSession gameSession2 = ActiveGame.GetSession("test");
+            //Assert
+            Assert.AreSame(gameSession1, gameSession2);
         }
     }
 }
